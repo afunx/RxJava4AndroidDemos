2071da8597fab18d0e63cae80bfb1fc8ac065cfe
白桦 <afunx.bai@ubtrobot.com>
2017-10-21 15:56:57 +0800

    observable.Creating.Create is modified

--- app/src/main/java/com/afunx/rxjava4androiddemos/samples/observable/Creating/Create.java
+++ app/src/main/java/com/afunx/rxjava4androiddemos/samples/observable/Creating/Create.java
@@ -13,6 +13,8 @@ import io.reactivex.functions.Consumer;
 
 /**
  * Created by afunx on 21/10/2017.
+ * <p>
+ * create an Observable from scratch by calling observer methods programmatically
  */
 
 public class Create extends BaseSample {
@@ -32,7 +34,7 @@ public class Create extends BaseSample {
         Observable.create(new ObservableOnSubscribe<String>() {
             @Override
             public void subscribe(@NonNull ObservableEmitter<String> e) throws Exception {
-                for (int i = 0; i < 10; i++) {
+                for (int i = 0; i < 3; i++) {
                     e.onNext(String.valueOf(i));
                 }
             }
@@ -50,8 +52,8 @@ public class Create extends BaseSample {
         Observable.create(new ObservableOnSubscribe<String>() {
             @Override
             public void subscribe(@NonNull ObservableEmitter<String> e) throws Exception {
-                for (int i = 0; i < 10; i++) {
-                    if (i == 5) {
+                for (int i = 0; i < 3; i++) {
+                    if (i == 1) {
                         e.onError(new IllegalStateException("Just for test"));
                     }
                     e.onNext(String.valueOf(i));
@@ -76,7 +78,7 @@ public class Create extends BaseSample {
         Observable.create(new ObservableOnSubscribe<String>() {
             @Override
             public void subscribe(@NonNull ObservableEmitter<String> e) throws Exception {
-                for (int i = 0; i < 10; i++) {
+                for (int i = 0; i < 3; i++) {
                     e.onNext(String.valueOf(i));
                 }
                 e.onComplete();
@@ -97,6 +99,59 @@ public class Create extends BaseSample {
                 Log.d(TAG, "Action run() for onComplete()");
             }
         });
+    }
+
+    @Override
+    public void test3() {
+        Log.i(TAG, "test3() Create simple demo, onNext() twice");
+        Observable<String> observable = Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<String> e) throws Exception {
+                for (int i = 0; i < 3; i++) {
+                    e.onNext(String.valueOf(i));
+                }
+            }
+        });
+        for (int time = 0; time < 2; time++) {
+            observable.subscribe(new Consumer<String>() {
+                @Override
+                public void accept(String s) throws Exception {
+                    Log.d(TAG, "Consumer<String> accept() s: " + s);
+                }
+            });
+        }
+    }
+
+    @Override
+    public void test4() {
+        Log.i(TAG, "test4() Create simple demo, onNext() and onComplete() twice");
+        Observable<String> observable = Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<String> e) throws Exception {
+                for (int i = 0; i < 3; i++) {
+                    e.onNext(String.valueOf(i));
+                }
+                e.onComplete();
+            }
+        });
+        for (int time = 0; time < 2; time++) {
+            observable.subscribe(new Consumer<String>() {
+                @Override
+                public void accept(String s) throws Exception {
+                    Log.d(TAG, "Consumer<String> accept() s: " + s);
+                }
+            }, new Consumer<Throwable>() {
+                @Override
+                public void accept(Throwable throwable) throws Exception {
+                    Log.d(TAG, "Consumer<Throwable> accept() throwable: " + throwable);
+                }
+            }, new Action() {
+                @Override
+                public void run() throws Exception {
+                    Log.d(TAG, "Action run() for onComplete()");
+                }
+            });
+        }
     }
 
     private static class SingletonHolder {
